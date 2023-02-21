a574a3deb071ba271515a88dc7fca245295dc1f1
root <root@zeeka-testnet-10.8.8.8.8>
2023-02-21 06:46:20 +0000

    Init

--- zeekaupdate.sh
+++ zeekaupdate.sh
@@ -13,7 +13,7 @@ rm /root/.bazuka-wallet
 echo "Wallet deleted"
 sleep 3
 
-bazuka init --network pelmeni-5 --mnemonic "pink crazy average laptop bus pond screen despair maximum smooth seat sun" --external 178.208.252.54:8765 --bootstrap 65.108.193.133:8765 --bootstrap 213.14.138.127:8765
+bazuka init --network pelmeni-7 --mnemonic "YOU_SEED" --external <YOU_EXTERNAL_IP>:8765 --bootstrap 65.108.193.133:8765 --bootstrap 213.14.138.127:8765
 
 service zeeka start
 journalctl -fn 100 -u zeeka
