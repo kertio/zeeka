c0f00533a764193e9f69808a16d6ddeebf87c347
kertio <_root_@list.ru>
2023-03-15 22:20:27 +0500

    Update zeekaupdate.sh

--- zeekaupdate.sh
+++ zeekaupdate.sh
@@ -13,7 +13,7 @@ rm /root/.bazuka-wallet
 echo "Wallet deleted"
 sleep 3
 
-bazuka init --network NET_NAME --mnemonic "YOU_SEED" --external <YOU_EXTERNAL_IP>:8765 --bootstrap 65.108.193.133:8765 --bootstrap 213.14.138.127:8765
+bazuka init --network <NET_NAME> --mnemonic <YOU_SEED> --external <YOU_EXTERNAL_IP>:8765 --bootstrap <BOOTSTRAP_IP>:8765 
 
 service zeeka start
 journalctl -fn 100 -u zeeka
